31711eff2be8d2cb2baf8ad1cabdb9e41903b6d6
Denis Krivosheev <denis.takeda@gmail.com>
2020-01-28 11:05:04 +0300

    Extend model and introduce session API

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, div, text)
+import Http
+import Json.Decode as Decode exposing (Decoder)
 
 
 main : Program () Model Msg
@@ -14,29 +16,87 @@ main =
         }
 
 
-type alias Model =
-    ()
+type Model
+    = Loading
+    | Unauthorised LoginData
+    | Authorised Session
+    | ApiUnavailable
 
 
-type alias Msg =
-    ()
+type alias LoginData =
+    { username : String
+    , password : String
+    }
+
+
+type alias Session =
+    { id : Int
+    , username : String
+    }
+
+
+type Msg
+    = GotSession (Result Http.Error Session)
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( (), Cmd.none )
+    ( Loading, requestSession )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update _ _ =
-    ( (), Cmd.none )
+update msg _ =
+    case msg of
+        GotSession (Ok session) ->
+            ( Authorised session, Cmd.none )
+
+        GotSession (Err (Http.BadStatus 401)) ->
+            ( Unauthorised { username = "", password = "" }, Cmd.none )
+
+        GotSession (Err _) ->
+            ( ApiUnavailable , Cmd.none )
 
 
 view : Model -> Html Msg
-view _ =
-    div [] [ text "Nothing yet" ]
+view model =
+    case model of
+        Loading ->
+            div [] [ text "Loading" ]
+
+        Unauthorised _ ->
+            div [] [ text "Unauthorised" ]
+
+        Authorised _ ->
+            div [] [ text "Authorised" ]
+
+        ApiUnavailable ->
+            div [] [ text "API unavailable" ]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
+
+
+
+-- Api --
+
+
+apiUrl : String
+apiUrl =
+    "/api"
+
+
+requestSession : Cmd Msg
+requestSession =
+    Http.get
+        { url = apiUrl ++ "/auth/session"
+        , expect = Http.expectJson GotSession sessionDecoder
+        }
+
+
+sessionDecoder : Decoder Session
+sessionDecoder =
+    Decode.map2 Session
+        (Decode.field "id" Decode.int)
+        (Decode.field "username" Decode.string)
